d8a1e64268b6f2b70b76822d3e1dd05093f77c11
pauli <paulinofonsecass@gmail.com>
2020-05-17 09:03:09 +0100

    criando a home

--- lib/main.dart
+++ lib/main.dart
@@ -30,61 +30,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _c,
-                    decoration: InputDecoration(
-                      hintText: "url",
-                      hintStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                          width: 1,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(90),
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(90),
-                    ),
-                    child: RaisedButton(
-                      onPressed: () async {
-                        _controller.future.then((value) async {
-                          await value.loadUrl('http://${_c.text}');
-                        });
-                      },
-                      child: Icon(Icons.search),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          body: WebView(
-            initialUrl: "http://www.google.com",
-            onWebViewCreated: (controller) {
-              _controller.complete(controller);
-              //a
-            },
-            onPageStarted: (s){
-              print('msg->$s');
-            },
-          ),
-        ),
-      ),
+      home: Home(),
     );
   }
 }
